daa65176d3d5fedd769b5846b305575376f18704
Tyler Johnston <tylergarrettjohnston@gmail.com>
2023-03-02 00:39:14 +0000

    Initial commit

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -41,9 +41,10 @@ public class Lab {
      */
     public String returnNumbers(int start, int end){
         String buildingString = "";
+        for (int i = start; i < end; i++) {
+            buildingString = buildingString + i +  " ";
+        }
 
-//        code here
-
-        return buildingString;
+        return buildingString.trim();
     }
 }
